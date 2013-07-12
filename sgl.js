b81f68fe0929014067404e6ed2d7f2969d217aab
Guy Pursey <guypursey@gmail.com>
2013-07-12 19:17:07 +0100

    Created a working single-server file and added an event-handler for reports to go to the console to the simple user interface. Issues-wise, this should close #24.

---
+++ sgl.js
@@ -0,0 +1,82 @@
+var express = require('express'),
+    app = express(),
+	server = require('http').createServer(app),
+    io = require('socket.io').listen(server), // 
+	ioc = require('socket.io-client'),
+	bot = require('./bot-client/beth.js'), // load Beth file
+	lib = require('./bot-client/lib/beth-eliza-orig.json'), // load the library
+	nme = 'Beth';
+	
+ // 8374 === BETH || BETA
+server.listen(8374);
+
+// On call, serve simple user interface file.
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/usr-client/ui-simple.html');
+});
+
+// Use the libraries folder.
+app.use('/lib', express.static(__dirname + '/usr-client/lib'));
+
+// Set up Beth using the constructor provided by the model and the data from the library.
+
+
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+	
+	var debugFn = function (msg) {
+			io.sockets.emit("report", msg);
+		},
+		postMsg = function (input) {
+			io.sockets.emit('updatedisplay', 'Beth', input, new Date());
+		},
+		severFn = function () {
+			delete usernames[socket.username];
+			//process.exit();
+			// update list of users in chat, client-side
+			io.sockets.emit('updateusers', usernames);
+			// echo globally that this client has left
+			socket.broadcast.emit('updatedisplay', 'SERVER', 'Beth has disconnected', new Date());
+		},
+		botobj = new bot.BotObj(true, lib.data, postMsg, severFn, debugFn);
+	
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (data) {
+		console.log(data);
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.emit('updatedisplay', socket.username, data, new Date());
+		if (socket.username !== nme && socket.username !== 'SERVER') {
+			// is there a better model than this conditional to stop eliza from looping?
+			botobj.transform(data);
+			//setTimeout(function () { clientsocket.emit('sendchat', botobj.transform(data)) }, 2000);
+		}
+	});
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		var datetime = new Date(),
+			datetimeStr = '[' + datetime.getDate() + '/' + (+datetime.getMonth() + 1) + '/' +
+			datetime.getFullYear() + ' ' + datetime.toLocaleTimeString() + ']';
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatedisplay', 'SERVER', 'you have connected', datetimeStr);
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatedisplay', 'SERVER', username + ' has connected', datetimeStr);
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatedisplay', 'SERVER', socket.username + ' has disconnected', new Date());
+	});
+});

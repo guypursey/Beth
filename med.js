a454e278ef856a7011633328797b6afdd8e4c7a5
Guy Pursey <guypursey@gmail.com>
2013-08-20 10:10:53 +0100

    Added code to for storing conversation as file.

--- med.js
+++ med.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     app = express(),
 	server = require('http').createServer(app),
+	fs = require('fs'),
+	pathname = '/dat/',
+	filename,
+	line_break = '\r\n',
     io = require('socket.io').listen(server);
 
 server.listen(8374); // 8374 === BETH || BETA
@@ -22,10 +26,23 @@ io.sockets.on('connection', function (socket) {
 		console.log(data);
 		// we tell the client to execute 'updatechat' with 2 parameters
 		io.sockets.emit('updatedisplay', socket.username, data, new Date());
+
+		// Set up new date in parseable format.
+		var d = new Date(),
+			dt = "[" + d.toLocaleDateString() + " " + d.toTimeString().replace(/\s.*$/, "") + "]",
+			save_msg = dt + " " + socket.username + ": " + data + line_break;
+
+		// Save the latest message to the file.
+		if (filename) {
+			fs.appendFile(filename, save_msg, function (err) {
+			if (err) throw err;
+				console.log('Saved message to ' + filename + ' successfully.', 'Ready to be read now.');
+			});
+		}
 	});
 
 	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
+	socket.on('adduser', function(username) {
 		var datetime = new Date(),
 			datetimeStr = '[' + datetime.getDate() + '/' + (+datetime.getMonth() + 1) + '/' +
 			datetime.getFullYear() + ' ' + datetime.toLocaleTimeString() + ']';
@@ -39,6 +56,12 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.emit('updatedisplay', 'SERVER', username + ' has connected', datetimeStr);
 		// update the list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
+
+		// If bot has logged in, create file...
+		if (username !== "user") {
+			filename = __dirname + pathname + username + "_anon_" + (new Date()).toISOString().replace(/\W+/g, "").replace(/T\d+Z$/, "") + ".txt";
+		}
+
 	});
 
 	// when the user disconnects.. perform this
